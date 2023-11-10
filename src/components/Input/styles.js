e108990b25083bac162a95a043b767692a0ba2fe
Carlos Fernandes <ceaf.sp@gmail.com>
2023-11-10 18:06:01 -0300

    Ajustes

---
+++ src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-itens: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFFAA;
+        border: 0;
+        
+        display: block;
+        direction: ltr;
+
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #000000;
+
+
+    }
+
+    `
